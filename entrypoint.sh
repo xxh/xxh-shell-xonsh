a912b2790139d4f99c277111f1f9bfb2a39eca6e
anki-code <anki-code>
2020-04-04 22:50:09 +0300

    0.7.11

--- entrypoint.sh
+++ entrypoint.sh
@@ -123,4 +123,14 @@ export XDG_CONFIG_HOME=$XDGPATH/.config
 export XDG_DATA_HOME=$XDGPATH/.local/share
 export XDG_CACHE_HOME=$XDGPATH/.cache
 
+for pluginrc_file in $(find $CURRENT_DIR/../../../plugins/xxh-plugin-prerun-*/build -type f -name '*pluginrc.sh' -printf '%f\t%p\n' 2>/dev/null | sort -k1 | cut -f2); do
+  if [[ -f $pluginrc_file ]]; then
+    if [[ $XXH_VERBOSE == '1' ]]; then
+      echo Load plugin $pluginrc_file
+    fi
+    #cd $(dirname $pluginrc_file)
+    source $pluginrc_file
+  fi
+done
+
 ./xonsh --no-script-cache -i --rc xonshrc.xsh $EXECUTE_FILE "${EXECUTE_COMMAND[@]}"
