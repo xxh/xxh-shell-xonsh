b8f513e605df4b6a83ee347df4cfb2b24e20670d
Andy Kipp <anki-code@users.noreply.github.com>
2025-02-26 12:59:28 +0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ do
   esac
 done
 
-url='https://github.com/xonsh/xonsh/releases/download/0.19.1/xonsh-x86_64.AppImage'
+url='https://github.com/xonsh/xonsh/releases/download/0.19.2/xonsh-x86_64.AppImage'
 appimage_name='xonsh'
 
 rm -rf $build_dir
