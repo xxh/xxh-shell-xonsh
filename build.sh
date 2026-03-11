0674c7d42b547f16dc079b50278fd4793eab9980
Andy Kipp <anki-code@users.noreply.github.com>
2026-03-11 08:28:31 +0600

    Update xonsh AppImage URL to version 0.22.7

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ do
   esac
 done
 
-url='https://github.com/xonsh/xonsh/releases/download/0.22.6/xonsh-x86_64.AppImage'
+url='https://github.com/xonsh/xonsh/releases/download/0.22.7/xonsh-x86_64.AppImage'
 appimage_name='xonsh'
 
 rm -rf $build_dir
